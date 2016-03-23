489bcd9ed28be94ea59e36a07686ad99f08cc0bd
Krett12 <krett12krett12@outlook.com>
2016-03-23 09:07:26 -0700

    jvvg says this is the plan
    I *may* have a workaround, but I'll do this for now.

--- app_config/cache/language/English/profile.php
+++ app_config/cache/language/English/profile.php
@@ -1,8 +1,8 @@
 <?php
 $lang_addl = array (
   'security' => 'Account security',
-  'sendPM' => 'Send the user a private message',
-  'sendas_admin' => 'Send as admin-issued warning',
+  'sendPM' => 'Send the user a private message (DEPRACATED). USE THE MAIN SITE SYSTEM',
+  'sendas_admin' => 'Send as admin-issued warning (DEPRACATED). USE THE MAIN SITE SYSTEM.',
   'pm_reply_header' => 'Replying to message at $1:',
   'showallposts' => 'Show all posts',
   'signoutothersessions' => 'Sign out all other sessions',
